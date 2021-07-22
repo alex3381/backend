b66ffd5c1c1866e4f1b27b982b2012333cc421ff
Alex Fatogun <alex.fatogun@yahoo.com>
2021-07-22 14:18:03 +0100

    My First Commit

---
+++ authentications/models.py
@@ -0,0 +1,77 @@
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
+from django.db import models
+from django.contrib.auth.models import (PermissionsMixin, AbstractBaseUser, BaseUserManager)
+from rest_framework_simplejwt.tokens import RefreshToken
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, id, email, password=None, password_confirmation=None, **extra_fields):
+
+        if email is None:
+            raise TypeError('Users should have an Email')
+
+        if password_confirmation is None:
+            raise TypeError('Users should have a id')
+
+        user = self.model(id=id, email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.set_password(password_confirmation)
+
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, id, email, password, password_confirmation, **extra_fields):
+        if password is None:
+            raise TypeError('Password should not be none')
+
+        if password_confirmation is None:
+            raise TypeError('You must Confirm Password')
+
+        user = self.create_user(id, email, password=password, password_confirmation=password_confirmation,
+                                **extra_fields)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.admin = True
+        user.save(using=self._db)
+
+        return user
+
+
+# AUTH_PROVIDERS = {'facebook': 'facebook', 'google': 'google',
+#                   'twitter': 'twitter', 'email': 'email'}
+
+
+# auth_provider = models.CharField(
+#     max_length=255, blank=False,
+#     null=False, default=AUTH_PROVIDERS.get('email'))
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.BigAutoField(primary_key=True, )
+    email = models.EmailField(max_length=150, unique=True, db_index=True)
+    password_confirmation = models.CharField(max_length=8,)
+    name = models.CharField(max_length=150, blank=True)
+    type = models.CharField(_('type'), max_length=150, blank=True)
+    is_verified = models.BooleanField(default=False),
+    is_active = models.BooleanField(default=True),
+    is_staff = models.BooleanField(default=False),
+    created_at = models.DateTimeField(auto_now_add=True),
+    updated_at = models.DateTimeField(auto_now=True),
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [ 'id', 'name', 'password', 'password_confirmation','type', ]
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
---
+++ authentications/serializers.py
@@ -0,0 +1,152 @@
+import django.contrib.auth.password_validation as validators
+from django.contrib import auth
+from django.contrib.auth import get_user_model
+from django.core import exceptions
+from django.core.validators import RegexValidator
+from django.forms import forms
+from phonenumber_field.serializerfields import PhoneNumberField
+from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework_jwt.serializers import jwt_payload_handler
+from rest_framework_jwt.settings import api_settings
+from rest_framework_simplejwt.tokens import RefreshToken
+from sqlparse.compat import text_type
+from authentications import models
+
+UserModel = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    tokens = serializers.SerializerMethodField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        max_length=128,
+        min_length=6,
+        write_only=True,
+        label="Password"
+
+    )
+    confirm_password = serializers.CharField(
+        style={'input_type': 'password'},
+        label="Confirm Password",
+        max_length=128, min_length=6, write_only=True
+
+    )
+
+    # is_staff = serializers.BooleanField(
+    #     label="Member of the team",
+    #     help_text="Indicates which user is able to access the administration site."
+    # )
+    #
+    # is_superuser = serializers.BooleanField(
+    #     label="superuser",
+    #     help_text="Indicates that this user has all permissions without explicitly assigning them."
+    #
+    # )
+
+    class Meta:
+        model = models.User
+        fields = ( 'email', 'password', 'confirm_password', 'type', 'tokens', )
+        extra_kwargs = {
+            'password': {'write_only': True},
+
+        }
+
+    def get_tokens(self, user):
+        tokens = RefreshToken.for_user(user)
+        refresh = text_type(tokens)
+        access = text_type(tokens.access_token)
+        data = {
+            "refresh": refresh,
+            "access": access
+        }
+        return data
+
+    def create(self, validated_data):
+        user = models.User(
+            email=self.validated_data ['email'],
+            # is_staff=self.validated_data['is_staff'],
+            # is_superuser=self.validated_data['is_superuser']
+        )
+
+        password = self.validated_data['password']
+        confirm_password = self.validated_data['confirm_password']
+
+        if password != confirm_password:
+            raise serializers.ValidationError({'password': 'Passwords do not match.'})
+        user.set_password(password)
+        user.save()
+        return user
+        # user.set_password(validated_data [ 'password' ])
+        # user.set_password(validated_data [ 'confirm_password' ])
+        #
+        # if password != confirm_password:
+        #  raise serializers.ValidationError({'password': 'Passwords do not match.'})
+        #  user.save()
+        #
+        #     return user
+
+    # def get_tokens(self, user):
+    #     tokens = RefreshToken.for_user(user)
+    #     refresh = text_type(tokens)
+    #     access = text_type(tokens.access_token)
+    #     data = {
+    #         "refresh": refresh,
+    #         "access": access
+    #     }
+    #     return data
+
+    # def create(self, validated_data):
+    #     user = models.User(
+    #         email=validated_data [ 'email' ]
+    #     )
+    #     user.set_password(validated_data [ 'password' ])
+    #     user.set_password(validated_data [ 'confirm_password' ])
+    #     user.save()
+    #     return user
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length=255, min_length=5)
+    password = serializers.CharField(max_length=68, min_length=6, write_only=True)
+    # confirm_password = serializers.CharField(allow_blank=False, write_only=True)
+
+    tokens = serializers.SerializerMethodField(read_only=True)
+
+    def get_tokens(self, obj):
+        user = UserModel.objects.get(email=obj [ 'email' ])
+
+        return {
+            'refresh': user.tokens() [ 'refresh' ],
+            'access': user.tokens() [ 'access' ]
+        }
+
+    class Meta:
+        model = UserModel
+        fields = [ 'id', 'email', 'tokens', 'password', 'type' ]
+
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        password = attrs.get('password', '')
+        user = auth.authenticate(email=email, password=password, )
+
+        if not user:
+            raise AuthenticationFailed('Invalid Credentials Provided, try again')
+
+        if not user.is_active:
+            raise AuthenticationFailed('Account Disabled, Contact Support')
+
+        if not user.is_verified:
+            raise AuthenticationFailed('Email is not Verified')
+        return {
+            'email': user.email,
+            'id': user.id,
+            'type': user.type,
+            'tokens': user.tokens,
+            # 'confirm_password' : user.confirm_password,
+
+        }
+
+        return super().validate(attrs),
+
+    #
